330ea93d7ab8fd1634ccac8468444977a3990cac
April Schuppel <36281897+aprilschuppel@users.noreply.github.com>
2025-01-09 13:43:25 -0500

    updating ' to &apos; in text

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ import {
 import {
   Card,
   CardContent,
-  CardFooter,
   CardHeader
 } from "@/components/ui/card"
 
@@ -66,7 +65,7 @@ export default function Home() {
                 <br />
                 Drum January is an annual daily drumming challenge by April Schuppel. In years past, April has just performed a full drum cover or original groove each day.
                 <br/>
-                <br/>For 2025, Drum January has evolved to a daily guessing game. Each day, she plays a beat and gives people 24 hours to guess what song it's from. The following day, the previous day's song is revealed and a new beat is posted.
+                <br/>For 2025, Drum January has evolved to a daily guessing game. Each day, she plays a beat and gives people 24 hours to guess what song it&apos;s from. The following day, the previous day&apos;s song is revealed and a new beat is posted.
                 <br/>
                 <br/>To play along, follow April on Instagram at <a href="https://www.instagram.com/schupsss/">@schupsss</a> or YouTube at <a href="https://www.youtube.com/@aprilschuppel">@aprilschuppel</a>.
               </TableCaption>
