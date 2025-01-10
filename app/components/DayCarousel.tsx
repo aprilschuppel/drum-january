f4abc211ac02d9e701f35509b806e434a3ae499f
April Schuppel <36281897+aprilschuppel@users.noreply.github.com>
2025-01-10 14:58:20 -0500

    added carousel that cycles through days

---
+++ app/components/DayCarousel.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+
+import { Card, CardContent, CardHeader } from "./ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+import Image from "next/image";
+import DailyGuesses from "./DailyGuesses";
+
+const videos = [
+  { day: 1, videoId: "baHqPF7GumQ" },
+  { day: 2, videoId: "36IhBy7xbAw" },
+  { day: 3, videoId: "8GQQAlQXdlU" },
+  { day: 4, videoId: "0B7qUH50yK0" },
+  { day: 5, videoId: "H1qC-39MIA0" },
+  { day: 6, videoId: "fOflCv3RdiU" },
+  { day: 7, videoId: "R5cFMQE5SdQ" },
+  { day: 8, videoId: "0t7NWfRaDq8" },
+  { day: 9, videoId: "LrtxOnu3DW8" }
+];
+
+export default function DayCarousel() {
+  const startIndex = videos.length - 1;
+
+  return (
+    <Carousel className="w-full" opts={{ loop: true, startIndex: startIndex}}>
+      <CarouselContent>
+        {Array.from({ length: videos.length }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+              <CardHeader className="text-center">
+                  <br />
+                  <Image className="self-center"
+                    aria-hidden
+                    src="/drum-january-logo-circle.png"
+                    alt="Drum January logo"
+                    width={200}
+                    height={200}
+                  />
+                  <br />
+                  <div className="text-2xl">Welcome to Day {videos[index].day}!</div>
+                  <br />
+                  {
+                    index === 0 ? (
+                      <p>Here&apos;s the first video of Drum January 2025!</p>
+                    ) : (
+                      <p>Here&apos;s the reveal of Day {videos[index].day - 1}&apos;s song and Day {videos[index].day}&apos;s beat to guess!</p>
+                    )
+                  }
+                  <br />
+                  <div className="flex justify-center w-full">
+                    <iframe
+                      src={`https://www.youtube.com/embed/${videos[index].videoId}`}
+                      title="YouTube video player"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                      className="w-full max-w-[560px] h-48 md:h-64 lg:h-96"
+                    ></iframe>
+                  </div>
+                </CardHeader>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  {
+                    index + 1 === videos.length ? (
+                      <p>There&apos;s still time to guess Day {index + 1}&apos;s beat!</p>
+                    ) : (
+                      <DailyGuesses day={index + 1 } />
+                    )
+                  }
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,11 @@
-import Image from "next/image";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import {
-  Card,
-  CardContent,
-  CardHeader
-} from "@/components/ui/card"
-import { fetchLatestGuesses } from "./lib/data";
-
-const guesses = await fetchLatestGuesses();
-
+import DayCarousel from "./components/DayCarousel";
 
 export default function Home() {
+  
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-2 md:p-10">
-      <div className="flex w-full md:max-w-[50%] sm:w-full flex-col gap-6">
-        <Card className="w-full">
-          <CardHeader className="text-center">
-            <br />
-            <Image className="self-center"
-              aria-hidden
-              src="/drum-january-logo-circle.png"
-              alt="Drum January logo"
-              width={200}
-              height={200}
-            />
-            <br />
-            <div className="flex justify-center w-full">
-              <iframe
-                src="https://www.youtube.com/embed/LrtxOnu3DW8"
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                className="w-full max-w-[560px] h-48 md:h-64 lg:h-96"
-              ></iframe>
-            </div>
-            <br />
-            Day 8 had {guesses.length} guesses!
-            <br />
-          </CardHeader>
-          <CardContent>
-            <Table className="w-full">
-              <TableCaption>
-                <br />
-                <br />
-                <br />
-                <hr />
-                <br />
-                Drum January is an annual daily drumming challenge by April Schuppel. In years past, April has just performed a full drum cover or original groove each day.
-                <br/>
-                <br/>For 2025, Drum January has evolved to a daily guessing game. Each day, she plays a beat and gives people 24 hours to guess what song it&apos;s from. The following day, the previous day&apos;s song is revealed and a new beat is posted.
-                <br/>
-                <br/>To play along, follow April on Instagram at <a href="https://www.instagram.com/schupsss/">@schupsss</a> or YouTube at <a href="https://www.youtube.com/@aprilschuppel">@aprilschuppel</a>.
-                <br/>
-              </TableCaption>
-              <TableHeader className="text-md">
-                <TableRow >
-                  <TableHead></TableHead>
-                  <TableHead className="font-bold">USERNAME</TableHead>
-                  <TableHead className="font-bold">GUESS</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody className="text-md">
-                {guesses.map((guess) => (
-                  <TableRow key={guess.username}>
-                  <TableCell>
-                    {guess.correct ? (
-                      <span aria-label="correct" role="img">
-                        🤘
-                      </span>
-                    ) : (
-                      <span aria-label="incorrect" role="img">
-                        👎
-                      </span>
-                    )}
-                  </TableCell>
-                    <TableCell className="font-large">{guess.username}</TableCell>
-                    <TableCell>{guess.guess}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </CardContent>
-        </Card>
+       <div className="flex w-full md:max-w-[50%] sm:w-full flex-col gap-6">
+        <DayCarousel />
       </div>
     </div>
   );
