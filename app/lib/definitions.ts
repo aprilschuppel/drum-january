a8fd40c885dfbf9f56e311edb53a8708362fbc79
April Schuppel <36281897+aprilschuppel@users.noreply.github.com>
2025-01-11 13:01:07 -0500

    updated videos to pull from database, added peaks of other cards in carousel, updated guess table styling

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -3,3 +3,16 @@ export type GuessTableRow = {
     guess: string;
     correct: boolean;
 };
+
+export type Video = {
+    id: number;
+    beatId: number;
+    postDate: string;
+    songName: string;
+    songArtist: string;
+    songDrummer: string;
+    youTubeId: string;
+    genre: string;
+    releaseYear: number;
+    peakOnBillboardChart: number;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import DayCarousel from "./components/DayCarousel";
 export default function Home() {
   
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-2 md:p-10">
-       <div className="flex w-full md:max-w-[50%] sm:w-full flex-col gap-6">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-6 bg-muted p-2 md:p-10">
+       <div className="flex w-full md:max-w-[50%] sm:w-full h-screen md:h-auto flex-col gap-6">
         <DayCarousel />
       </div>
     </div>
