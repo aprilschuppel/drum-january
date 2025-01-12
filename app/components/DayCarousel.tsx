e547f117915e24fb9af45c50fa2b2f9e1f9f49a3
April Schuppel <36281897+aprilschuppel@users.noreply.github.com>
2025-01-12 14:57:36 -0500

    Add stats page
    Stats page with leaderboard and song stats

--- app/components/DayCarousel.tsx
+++ app/components/DayCarousel.tsx
@@ -19,7 +19,8 @@ export default function DayCarousel() {
     <Carousel className="w-full h-screen" opts={{ loop: true, startIndex: startIndex}}>
       <CarouselContent>
         {Array.from({ length: videos.length }).map((_, index) => (
-          <CarouselItem key={index} className={`${index === index ? 'basis-4/5' : 'basis-1/10'}`}>
+          <CarouselItem key={index} className={`w-full ${index === index ? 'basis-4/5' : 'basis-1/10'}`}
+          >
             <div>
               <Card className="h-full md:h-auto">
               <CardHeader className="text-center">
@@ -33,9 +34,9 @@ export default function DayCarousel() {
                   <div className="text-2xl">Welcome to Day {videos[index].beatId}!</div>
                   {
                     index === 0 ? (
-                      <p>Here&apos;s the first video of Drum January 2025!</p>
+                      <p className="text-sm">Here&apos;s the first video of Drum January 2025!</p>
                     ) : (
-                      <p>Here&apos;s the reveal of Day {videos[index].beatId - 1}&apos;s song and Day {videos[index].beatId}&apos;s beat to guess!</p>
+                      <p className="text-sm">Here&apos;s the reveal of Day {videos[index].beatId - 1}&apos;s song and Day {videos[index].beatId}&apos;s beat to guess!</p>
                     )
                   }
                   <div className="flex justify-center w-full">
@@ -50,7 +51,19 @@ export default function DayCarousel() {
                   <br/>
                   {
                     index + 1 === videos.length ? (
-                      <p className="top font-bold">There&apos;s still time to guess Day {index + 1}&apos;s beat!</p>
+                      <>
+                        <p className="top font-bold">There&apos;s still time to guess Day {index + 1}&apos;s beat!</p>
+                        <br />
+                        <hr />
+                        <br />
+                        <div className="text-xs text-gray-500">
+                          Drum January is an annual daily drumming challenge by April Schuppel. <br/>
+                          <br />For 2025, Drum January has evolved to a daily guessing game. Each day, she plays a beat and gives people 24 hours to guess what song it&apos;s from. The following day, the previous day&apos;s song is revealed and a new beat is posted.
+                          <br />
+                          <br />To play along, follow April on Instagram at <a href="https://www.instagram.com/schupsss/">@schupsss</a> or YouTube at <a href="https://www.youtube.com/@aprilschuppel">@aprilschuppel</a>.
+                          <br />
+                        </div>
+                      </>
                     ) : (
                       <div className="top font-bold">Day {index + 1} Guesses<br/><br/><hr/></div>
                       
---
+++ app/components/Footer.tsx
@@ -0,0 +1,15 @@
+
+
+export function SiteFooter() {
+  return (
+    <footer className="border-grid border-t py-6 md:px-8 md:py-0">
+      <div className="container-wrapper">
+        <div className="container py-4">
+          <div className="text-center text-xs leading-loose text-muted-foreground md:text-left">
+            To play along, follow April on Instagram at <a href="https://www.instagram.com/schupsss/">@schupsss</a> or YouTube at <a href="https://www.youtube.com/@aprilschuppel">@aprilschuppel</a>.
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
