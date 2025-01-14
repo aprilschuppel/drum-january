89f09576c23f431b8ee9464953cb136d6752f580
April Schuppel <36281897+aprilschuppel@users.noreply.github.com>
2025-01-14 16:08:25 -0500

    adds ability to guess the song

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -6,7 +6,7 @@ import { SiteFooter } from "../components/Footer";
 export default function Home() {
   
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-6 bg-muted p-2 md:p-10">
+    <div className="flex min-h-screen flex-col items-center gap-6 bg-muted p-2 md:p-10">
        <div className="flex w-full md:max-w-[50%] sm:w-full h-screen md:h-auto flex-col gap-6">
        <Link key='home' href='/'
         className="flex h-2 mb-2 grow items-center justify-center p-3 font-medium text-blue-700 text-center">  
---
+++ app/thanks/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../components/ui/card"
+
+export default function Page() {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-top bg-muted p-16">
+            <Card>
+                    <CardHeader className="justify-center items-center">
+                        <CardTitle className="text-xl">Thanks for submitting!</CardTitle>
+                        <CardDescription><br/>Check back for the reveal and tomorrow's challenge.</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                    <Link key='home' href='/' className="flex h-2 mb-2 grow items-center justify-center p-3 text-blue-700 text-center">  
+                        Back to the home page
+                    </Link>
+                    </CardContent>
+                </Card>
+        </div>
+    );
+}
