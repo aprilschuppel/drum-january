9acb12c406c924512b9e1511a49e93822cd7ad8a
April Schuppel <36281897+aprilschuppel@users.noreply.github.com>
2025-01-24 18:18:20 -0500

    update pages

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -4,12 +4,14 @@ import {
 } from "../components/LeaderboardComponents";
 import { Card } from "../components/ui/card";
 
-export default function TestPage() {
+export default function LeaderboardPage() {
   return (
+    <div className="w-full h-full">
       <Card className="bg-white w-4/5 justify-self-center h-auto space-y-2 overflow-scroll p-4">
         <DailyCorrectTracker />
         <div className="h-2" />
         <OverallLeaders />
       </Card>
+      </div>
   );
 }
--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -3,11 +3,13 @@ import { songs } from "@/src/data/data";
 import { columns } from "@/src/components/data-table/songColumns";
 import { Card } from "../components/ui/card";
 
-export default function Home() {
+export default function SongStatsPage() {
   return (
-    <Card className="bg-white w-11/12 justify-self-center h-auto space-y-2 overflow-scroll p-2">
-      <h3 className="mb-1 font-bold">Song Stats</h3>
-      <DataTable columns={columns} data={songs} />
-    </Card>
+    <div className="w-full h-full">
+      <Card className="bg-white w-11/12 justify-self-center h-auto space-y-2 overflow-scroll p-2">
+        <h3 className="mb-1 font-bold">Song Stats</h3>
+        <DataTable columns={columns} data={songs} />
+      </Card>
+    </div>
   );
 }
