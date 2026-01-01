7f958d6419583137d257d9e876ed075ad8b0e64f
April Schuppel <36281897+aprilschuppel@users.noreply.github.com>
2026-01-01 13:05:57 -0500

    added auth and guess form (#10)
    * added auth and guess form
    
    * remove pnpm lockfile to fix vercel build
    
    * fix linting errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
+// src/app/layout.tsx
 import type { Metadata } from "next";
 import "./globals.css";
-import NavBar from "./components/NavBar";
-import { SiteFooter } from "./components/Footer";
+import { Providers } from "./providers";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Navbar, NavbarBrand } from "@heroui/navbar";
+import Image from "next/image";
+
 export const metadata: Metadata = {
   title: "Drum January",
   description: "April Schuppel's annual daily drumming challenge.",
@@ -13,12 +17,33 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="min-h-screen flex flex-col bg-palette-sunset-900 mt-24">
-        <NavBar />
-        <div className="py-4">{children}</div>
-        <SiteFooter />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className="min-h-screen flex flex-col bg-palette-sunset-900 text-white">
+          <Providers>
+            <Navbar
+              maxWidth="full"
+              position="sticky"
+              className="bg-palette-polynesian_blue"
+            >
+              <NavbarBrand>
+                <Image
+                  alt="Drum January Logo"
+                  src="/drum-january-logo-circle.png"
+                  width={64}
+                  height={64}
+                />
+                <p className="text-2xl font-bold text-inherit pl-2">Drum January</p>
+              </NavbarBrand>
+            </Navbar>
+
+            {/* Main Content */}
+            <main className="flex-grow container mx-auto px-1 py-2 md:px-6 md:py-8">
+              {children}
+            </main>
+          </Providers>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
